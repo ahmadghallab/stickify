b4f29b3e46f4215cc92e737a62c70cc899e8531d
Ahmad Ghallab <help.ahmadghallab@gmail.com>
2019-04-16 17:34:50 +0200

    Card transition

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,19 @@
 <template>
-  <div>
+  <div class="border-bottom">
     <div class="row justify-content-between bootstrap-nav">
       <div class="col-auto align-self-center px-0">
         <h4 class="mb-0 font-weight-bold">
-          <router-link :to="{ name: 'home' }">myCards</router-link></h4>
+          <router-link :to="{ name: 'home' }">inCard</router-link></h4>
       </div>
       <div class="col-auto align-self-center">
-        <router-link :to="{ name: 'studysets' }">Explore Study Sets</router-link>
+        <form>
+          <input type="text" class="form-control search-box" autocomplete="off" placeholder="Search studysets">
+        </form>
+      </div>
+      <div class="col-auto align-self-center">
+        <router-link :to="{ name: 'studysets' }">
+          <span class="circle circle-md grey text-white">ah</span>
+        </router-link>
       </div>
     </div>
   </div>
--- src/views/Cards.vue
+++ src/views/Cards.vue
@@ -41,18 +41,14 @@
       <Loader v-if="listCardsLoader" />
       <div v-else>
         <div v-if="cards.length">
-          <transition name="flip" mode="out-in">
-            <div v-if="flipTermDefinition" key="1">
+          <transition name="card-slide" mode="out-in">
+            <div :key="cardIdx">
               <div class="card__header"></div>
               <div class="card__body text-white flashcard d-flex justify-content-center" v-bind:class="cards[cardIdx].color">
-                <h2 class="align-self-center">{{ cards[cardIdx].definition }}</h2>
-              </div>
-              <div class="card__footer"></div>
-            </div>
-            <div v-else key="2">
-              <div class="card__header"></div>
-              <div class="card__body text-white flashcard d-flex justify-content-center" v-bind:class="cards[cardIdx].color">
-                <h1 class="align-self-center">{{ cards[cardIdx].term }}</h1>
+                <transition name="card-slide" mode="out-in">
+                  <h2 class="align-self-center" v-if="flipTermDefinition" key="1">{{ cards[cardIdx].definition }}</h2>
+                  <h1 class="align-self-center text-center" v-else key="2">{{ cards[cardIdx].term }}</h1>
+                </transition>
               </div>
               <div class="card__footer"></div>
             </div>
@@ -60,9 +56,27 @@
           <div class="row justify-content-between mb-custom">
             <div class="col-auto align-self-center">
               <a href="javascript:void(0)" @click="changeCard('prev')"
-                class="circle circle-md green text-white mr-1">prev</a>
+                class="circle circle-md grey mr-1">
+                <svg class="icon" viewBox="0 0 512 512">
+                  <g>
+                    <path d="M379.644,477.872l-207.299-207.73c-7.798-7.798-7.798-20.486,0.015-28.299L379.643,34.128
+                      c7.803-7.819,7.789-20.482-0.029-28.284c-7.819-7.803-20.482-7.79-28.284,0.029L144.061,213.574
+                      c-23.394,23.394-23.394,61.459-0.015,84.838L351.33,506.127c3.907,3.915,9.031,5.873,14.157,5.873
+                      c5.111,0,10.224-1.948,14.128-5.844C387.433,498.354,387.446,485.691,379.644,477.872z"/>
+                  </g>
+                </svg>
+              </a>
               <a href="javascript:void(0)" @click="changeCard('next')"
-                class="circle circle-md green text-white ml-1">next</a>
+                class="circle circle-md grey ml-1">
+                <svg class="icon" viewBox="0 0 512 512">
+                  <g>
+                    <path d="M367.954,213.588L160.67,5.872c-7.804-7.819-20.467-7.831-28.284-0.029c-7.819,7.802-7.832,20.465-0.03,28.284
+                      l207.299,207.731c7.798,7.798,7.798,20.486-0.015,28.299L132.356,477.873c-7.802,7.819-7.789,20.482,0.03,28.284
+                      c3.903,3.896,9.016,5.843,14.127,5.843c5.125,0,10.25-1.958,14.157-5.873l207.269-207.701
+                      C391.333,275.032,391.333,236.967,367.954,213.588z"/>
+                  </g>
+                </svg>
+              </a>
             </div>
             <div class="col-auto align-self-center">
               <svg width="52" viewBox="0 0 72 72">
@@ -70,7 +84,7 @@
                   <use class="progress__value" stroke="#b7c0c7" stroke-width="5" transform="rotate(-90 36 36)" stroke-linecap="round" stroke-dashoffset="0" stroke-dasharray="201.06192982974676" xlink:href="#circle">
                     <circle id="circle" cx="36" cy="36" r="32" fill="#b7c0c7"></circle>
                   </use>
-                  <use class="progress__value" stroke="#4caf50" stroke-width="5" stroke-linecap="round" transform="rotate(-90 36 36)" stroke-dasharray="201.06192982974676" v-bind:stroke-dashoffset="cardProgress" xlink:href="#circle">
+                  <use class="progress__value" stroke="#fff" stroke-width="5" stroke-linecap="round" transform="rotate(-90 36 36)" stroke-dasharray="201.06192982974676" v-bind:stroke-dashoffset="cardProgress" xlink:href="#circle">
                     <circle id="circle" cx="36" cy="36" r="32" fill="none"></circle>
                   </use>
                   <text y="38" x="36" fill="#fff" font-size="18" text-anchor="middle" dominant-baseline="middle">{{ cardNo + '/' + cards.length }}</text>
@@ -112,9 +126,9 @@
                   </g>
                 </svg>
               </a>
-              <a href="javascript:void(0)" @click="flipTermDefinition = !flipTermDefinition" class="circle circle-md green">
-                <svg class="icon" viewBox="0 0 22 17">
-                  <path d="M.746 11.46l4.57 4.571v-3.398h7.97v-2.285h-7.97V6.949l-4.57 4.512zm15.938-7.968h-7.97v2.285h7.97v3.399l4.57-4.512-4.57-4.57v3.398z"></path>
+              <a href="javascript:void(0)" @click="flipTermDefinition = !flipTermDefinition" class="circle circle-md grey">
+                <svg class="icon" viewBox="0 0 512 512">
+                  <path d="m63.613281 190.464844c-11.371093-11.371094-17.613281-26.4375-17.613281-42.464844s6.242188-31.09375 17.574219-42.425781l100.324219-99.757813c7.832031-7.785156 20.496093-7.75 28.285156.082032 7.789062 7.832031 7.75 20.496093-.082032 28.28125l-100.285156 99.71875c-3.734375 3.738281-5.816406 8.757812-5.816406 14.101562 0 5.339844 2.082031 10.363281 5.859375 14.140625l100.242187 99.675781c7.832032 7.789063 7.871094 20.453125.082032 28.285156-3.910156 3.929688-9.046875 5.898438-14.183594 5.898438-5.101562 0-10.199219-1.9375-14.101562-5.816406zm428.386719-62.464844h-327c-11.046875 0-20 8.953125-20 20s8.953125 20 20 20h327c11.046875 0 20-8.953125 20-20s-8.953125-20-20-20zm-143.898438 93.816406c-7.832031-7.785156-20.496093-7.75-28.285156.082032-7.789062 7.832031-7.75 20.496093.082032 28.28125l100.242187 99.679687c3.777344 3.773437 5.859375 8.796875 5.859375 14.140625 0 5.339844-2.082031 10.363281-5.816406 14.101562l-100.285156 99.714844c-7.832032 7.789063-7.871094 20.453125-.082032 28.285156 3.910156 3.929688 9.046875 5.898438 14.183594 5.898438 5.097656 0 10.199219-1.9375 14.101562-5.816406l100.324219-99.757813c11.332031-11.332031 17.574219-26.398437 17.574219-42.425781s-6.242188-31.09375-17.613281-42.46875zm18.898438 142.183594c0-11.046875-8.953125-20-20-20h-327c-11.046875 0-20 8.953125-20 20s8.953125 20 20 20h327c11.046875 0 20-8.953125 20-20zm0 0"/>
                 </svg>
               </a>
             </div>
@@ -192,8 +206,10 @@ export default {
     changeCard (dir) {
       if (dir == 'next') {
         this.cardIdx = this.cardIdx + 1
+        this.cardNo = this.cardNo + 1
       } else {
         this.cardIdx = this.cardIdx - 1
+        this.cardNo = this.cardNo - 1
       }
     }
   },
@@ -204,25 +220,11 @@ export default {
 }
 </script>
 <style scoped>
-.flip-enter-active {
-  transition: all 0.2s cubic-bezier(0.55, 0.085, 0.68, 0.53);
+.card-slide-enter-active, .card-slide-leave-active {
+  transition: all .3s ease;
 }
-.flip-leave-active {
-  transition: all 0.25s cubic-bezier(0.25, 0.46, 0.45, 0.94);
-}
-.flip-enter, .flip-leave-to {
-  -webkit-transform: scaleY(0) translateZ(0);
-          transform: scaleY(0) translateZ(0);
+.card-slide-enter, .card-slide-leave-to {
+  transform: translateX(10px);
   opacity: 0;
-}
-.slide-leave-active,
-.slide-enter-active {
-  transition: 1s;
-}
-.slide-enter {
-  transform: translate(100%, 0);
-}
-.slide-leave-to {
-  transform: translate(-100%, 0);
 }
 </style>
